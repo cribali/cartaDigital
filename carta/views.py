0e6be52ce48380185d28d1837d1c17fdb425b214
cribali <cristianbalihaut@gmail.com>
2024-05-08 22:22:16 -0300

    actualizacion de views, sin funcionar aun

--- carta/views.py
+++ carta/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.utils.translation import activate
 from django.views.generic import TemplateView
-from .models import Bebida
+from .models import Item, Categoria
 
 def set_language(request):
     if request.method == 'POST':
@@ -12,10 +12,6 @@ def set_language(request):
             activate(language)
     return redirect(request.POST.get('next', '/'))
 
-def Bienvenida(request):
-    saraza= 'mierda que no queres traducir'
-    return render(request,'inicio.html',{'traductionPlis' : saraza })
-# Create your views here.
 
 def comensal(request):
     return render(request,'seleccionComensal.html')
@@ -23,15 +19,22 @@ def comensal(request):
 def menu(request):
     return render(request,'tipoMenu.html')
 
-class ItemController(TemplateView):
+class BebidaView(TemplateView):
     template_name="bebidas.html"
     def get_context_data(self, **kwargs):
         model = super().get_context_data(**kwargs)
-        model["bebidas"] = Bebida.objects.all()
+        model["bebidas"] = Item.objects.filter(categoria__nombre = "bebida")
         return model
     
     #bebidas= Bebida.objects.all()
     #return render(request,'bebidas.html',{'bebidas': bebidas})
+
+class BodegaView(TemplateView):
+    template_name="bodega.html"
+    def get_context_data(self, **kwargs):
+        model = super().get_context_data(**kwargs)
+        model["bodega"] = Item.objects.filter(categoria__nombre = "bodega")
+        return model
 
 def bodega(request):
     return render(request, 'bodega.html')
